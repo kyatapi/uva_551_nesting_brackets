bc5a0cd3bd598dfe516eed14da43682bad4b9c91
mike_su <mike_su@nuuo.com>
2017-05-24 13:17:45 +0800

    Add function for matching open/close brackets

--- nesting_brackets.cpp
+++ nesting_brackets.cpp
@@ -9,8 +9,36 @@ inline void run_test(int argc, char **argv) {}
 
 using namespace std;
 
+const pair<string, string> supported_brackets[] = {
+	pair<string, string>("(*", "*)"),
+	pair<string, string>("(", ")"),
+	pair<string, string>("[", "]"),
+	pair<string, string>("{", "}"),
+	pair<string, string>("<", ">")
+};
+
+const size_t supported_brackets_count = 5;
+
 unsigned int find_offending_brackets(const string&  line) {
 	return 0;
+}
+
+int match_open_brackets(const string& line, const size_t start_from) {
+	for(size_t i = 0; i < supported_brackets_count; ++i) {
+		if (line.compare(start_from, supported_brackets[i].first.length(), supported_brackets[i].first) == 0) {
+			return i;
+		}
+	}
+	return -1;
+}
+
+int match_close_brackets(const string& line, const size_t start_from) {
+	for(size_t i = 0; i < supported_brackets_count; ++i) {
+		if (line.compare(start_from, supported_brackets[i].second.length(), supported_brackets[i].second) == 0) {
+			return i;
+		}
+	}
+	return -1;
 }
 
 int main(int argc, char **argv) {
--- test_cases.cpp
+++ test_cases.cpp
@@ -4,7 +4,37 @@
 using namespace std;
 
 extern unsigned int find_offending_brackets(const string&);
+extern int match_open_brackets(const string&, const size_t);
+extern int match_close_brackets(const string&, const size_t);
 
 TEST(FindOffendingBrackets, HandleEmptyLine) {
 	ASSERT_EQ(0, find_offending_brackets(""));
+}
+
+//MatchOpenBrackets
+TEST(MatchOpenBrackets, HandleEmptyLine) {
+	ASSERT_EQ(-1, match_open_brackets("", 0));
+}
+
+TEST(MatchOpenBrackets, HandleNormalBrackets) {
+	ASSERT_EQ(1, match_open_brackets("(", 0));
+	ASSERT_EQ(0, match_open_brackets("(*", 0));
+}
+
+TEST(MatchOpenBrackets, HandleNoBrackets) {
+	ASSERT_EQ(-1, match_open_brackets("abc", 0));
+}
+
+//MatchCloseBrackets
+TEST(MatchCloseBrackets, HandleEmptyLine) {
+	ASSERT_EQ(-1, match_close_brackets("", 0));
+}
+
+TEST(MatchCloseBrackets, HandleNormalBrackets) {
+	ASSERT_EQ(1, match_close_brackets(")", 0));
+	ASSERT_EQ(0, match_close_brackets("*)", 0));
+}
+
+TEST(MatchCloseBrackets, HandleNoBrackets) {
+	ASSERT_EQ(-1, match_close_brackets("abc", 0));
 }
